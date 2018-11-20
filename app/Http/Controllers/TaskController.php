b321d43e43471468788d763d2582921aa86ae101
3A532088 <paileo007007@gmail.com>
2018-11-20 19:09:49 +0800

    定義TaskController 的 index 方法，使其回覆views/tasks/index.blade.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class TaskController extends Controller
@@ -11,5 +10,16 @@ class TaskController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    /**
+     * 顯示使用者所有任務的清單。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('tasks.index');
     }
 }
