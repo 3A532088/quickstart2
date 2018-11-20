cd0c9fbfeed732082dfc0927f48c7fa62b8d5de2
3A532088 <paileo007007@gmail.com>
2018-11-20 19:29:49 +0800

    TaskController取得tasks資料後，交由tasks/index.blade.php呈現出來

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
-    public function __construct()
+    protected $tasks;
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
+
+        $this->tasks = $tasks;
     }
 
     /**
