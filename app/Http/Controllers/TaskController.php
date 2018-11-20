c00789019dd49e4a488f9c3aabe1686bf162d18e
3A532088 <paileo007007@gmail.com>
2018-11-20 20:09:26 +0800

    修正

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -18,12 +18,6 @@ class TaskController extends Controller
         $this->tasks = $tasks;
     }
 
-    /**
-     * 顯示使用者所有任務的清單。
-     *
-     * @param  Request  $request
-     * @return Response
-     */
     public function index(Request $request)
     {
         $tasks = Task::where('user_id', $request->user()->id)->get();
--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -10,15 +10,6 @@ class TaskPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
 
     public function destroy(User $user, Task $task)
     {
